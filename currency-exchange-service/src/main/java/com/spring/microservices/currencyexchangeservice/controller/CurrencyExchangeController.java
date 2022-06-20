9251fee7cf89eec6d23d2fb43683a212bda5ad36
Tripathi, Praveen Kumar <pravtripathi@deloitte.com>
2022-06-20 12:04:20 +0530

    connected to database

--- currency-exchange-service/src/main/java/com/spring/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/spring/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,13 +1,12 @@
 package com.spring.microservices.currencyexchangeservice.controller;
 
 import com.spring.microservices.currencyexchangeservice.model.CurrencyExchange;
+import com.spring.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
@@ -15,10 +14,19 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeRepository exchangeRepository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange =
+                exchangeRepository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " + from + "to " + to);
+        }
+
         /*
         this will return the server port from where result is fetching
          */
