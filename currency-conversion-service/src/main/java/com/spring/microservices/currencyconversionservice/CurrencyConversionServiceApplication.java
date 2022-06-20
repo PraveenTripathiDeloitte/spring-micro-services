6d582f066d7849ca7a3ae0920dfaa70469ea743d
Tripathi, Praveen Kumar <pravtripathi@deloitte.com>
2022-06-20 13:06:45 +0530

    invoked currency-exchange micro-service into CurrencyConversion using feign client

--- currency-conversion-service/src/main/java/com/spring/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/spring/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,13 @@ package com.spring.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+/*
+this will enable feign clients, and we will be able to talk with other micro-services
+ */
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- currency-conversion-service/src/main/java/com/spring/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/spring/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package com.spring.microservices.currencyconversionservice.controller;
 
 import com.spring.microservices.currencyconversionservice.model.CurrencyConversion;
+import com.spring.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,9 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    CurrencyExchangeProxy proxy;
 
     @GetMapping(path = "/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -33,5 +38,19 @@ public class CurrencyConversionController {
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
                 currencyConversion.getEnvironment());
+    }
+
+    @GetMapping(path = "/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity
+    ) {
+        /*
+          to get the body of currency conversion obtained from currency exchange micro-service
+         */
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + "" + "feign");
     }
 }
