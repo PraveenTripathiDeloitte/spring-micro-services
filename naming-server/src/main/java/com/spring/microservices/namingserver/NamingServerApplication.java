99c557cc91fae6fdf9b6d00b092be52d1338d3c4
Tripathi, Praveen Kumar <pravtripathi@deloitte.com>
2022-06-20 15:51:58 +0530

    set the eureka naming-server

--- naming-server/src/main/java/com/spring/microservices/namingserver/NamingServerApplication.java
+++ naming-server/src/main/java/com/spring/microservices/namingserver/NamingServerApplication.java
@@ -2,7 +2,12 @@ package com.spring.microservices.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
+/*
+to enable eureka server
+ */
 @SpringBootApplication
 public class NamingServerApplication {
 
