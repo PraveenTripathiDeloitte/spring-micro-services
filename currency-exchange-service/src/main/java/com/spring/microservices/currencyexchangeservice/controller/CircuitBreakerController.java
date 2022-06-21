2c4a893a90e35cfc239b7f45733d5d39b5008885
Tripathi, Praveen Kumar <pravtripathi@deloitte.com>
2022-06-21 13:24:01 +0530

    added circuit-breaker, retry and fallback features

---
+++ currency-exchange-service/src/main/java/com/spring/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,32 @@
+package com.spring.microservices.currencyexchangeservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+    public String sampleAPI() {
+        logger.info("Sample API call received");
+        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/some-dummy-url",
+                String.class);
+        return responseEntity.getBody();
+    }
+
+    /*
+    will return this response if after calling the API maximum times
+    no response is obtained
+     */
+    public String hardCodedResponse(Exception ex) {
+        return "fallback-response";
+    }
+}
